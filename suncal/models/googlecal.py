9a8c733fe3262e85317eae705da7eb3352274070
Franziska Witzel <franziska.witzel@gmail.com>
2021-02-23 19:51:05 +0100

    start writing BaseModel for google calendar start/end times

---
+++ suncal/models/googlecal.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, validator, root_validator
+from typing import Optional
+
+
+class GoogleCalEvent(BaseModel):
+    pass
+
+
+class GoogleCalTime(BaseModel):
+    """
+    Can fill the "start" or "end" required field of a GoogleCalEvent!
+    Has three fields: date, dateTime and timeZone, however, not all of them are required at a time.
+
+    date: "yyyy-mm-dd" for all day events
+    dateTime: isoformat, for timed events --> event needs either date OR dateTime!
+    timeZone: IANA timezone db name, NOT required if dateTime contains a time offset
+    """
+
+    date: Optional[str] = None
+    dateTime: Optional[str] = None
+    timeZone: Optional[str] = None
+
+    # make sure that either date OR dateTime is provided
+    @root_validator(pre=True)
+    def date_or_dateTime_provided(cls, values):
+        date, datetime = values.get('date'), values.get('dateTime')
+        assert date is None or datetime is None, "You have to provide a date for all day events or a datetime for " \
+                                                 "timed events"
+        return values
+
+
+if __name__ == "__main__":
+    # should throw an assertion error --> TODO: add this to tests!
+    gtime = GoogleCalTime(date="1", dateTime="2")
