27748b2b4f13c5cb8e27cde6678b1832fdf9b529
Franziska Witzel <franziska.witzel@gmail.com>
2021-04-17 14:35:40 +0200

    add test for creation of timezone aware datetimes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-from suncal.utils import date_range
+from suncal.utils import create_timezone_aware_datetime, date_range
 
 
 def test_date_range():
@@ -29,3 +29,26 @@ def test_date_range():
 
     out = [dt.date(2020, 12, 30)]
     assert out == date_range(from_date, to_date)
+
+
+def test_create_timezone_aware_datetime():
+    my_time = create_timezone_aware_datetime(
+        year=2021,
+        month=4,
+        day=17,
+        hour=16,
+        minute=30,
+        second=0,
+        timezone="Europe/Berlin",
+    )
+    assert isinstance(my_time, dt.datetime)
+    assert my_time.year == 2021
+    assert my_time.second == 0
+    assert my_time.month == 4
+    assert my_time.hour == 16
+    # "CET" in daylight saving time: "CEST"
+    assert my_time.tzname() == "CEST"
+    # utc offset of 2h because of daylight saving time
+    assert my_time.utcoffset() == dt.timedelta(seconds=7200)
+    # daylight saving time adjustment of 1h
+    assert my_time.dst() == dt.timedelta(seconds=3600)
