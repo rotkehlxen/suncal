6904c89fd4e09366c16f6f3f489342c31379de8d
Franziska Witzel <franziska.witzel@gmail.com>
2021-04-22 18:46:14 +0200

    first working version

--- suncal/capi-quickstart.py
+++ suncal/capi-quickstart.py
@@ -1,8 +1,10 @@
+import datetime as dt
 from typing import List
 
 from googleapiclient.discovery import build
 
 from suncal.auth import get_credentials
+from suncal.main import suncal
 
 # read and write access to google calendars AND calendar events:
 # TODO: this scope is a bit too extensive - check how to be more specific
@@ -44,19 +46,30 @@ creds = get_credentials(SCOPES)
 
 # Get all your calendars -----------------------------------------------------------------------------------------------
 # the result can potentially span several pages (i.e. there is a max number of entries per page!)
-with build("calendar", "v3", credentials=creds) as service:
-    calendars: List = []
-    page_token = None
-    while True:
-        calendar_list = (
-            service.calendarList().list(pageToken=page_token).execute()
-        )
-        calendars = calendars + [
-            calendar_list_entry["summary"]
-            for calendar_list_entry in calendar_list["items"]
-        ]
-        page_token = calendar_list.get("nextPageToken")
-        if not page_token:
-            break
+# with build("calendar", "v3", credentials=creds) as service:
+#     calendars: List = []
+#     page_token = None
+#     while True:
+#         calendar_list = (
+#             service.calendarList().list(pageToken=page_token).execute()
+#         )
+#         calendars = calendars + [
+#             calendar_list_entry["summary"]
+#             for calendar_list_entry in calendar_list["items"]
+#         ]
+#         page_token = calendar_list.get("nextPageToken")
+#         if not page_token:
+#             break
+#
+# print(calendars)
 
-print(calendars)
+# Test main of module --------------------------------------------------------------------------------------------------
+# suncal(calendar_title="Sonne",
+#        from_date=dt.date(2021, 4, 22),
+#        to_date=dt.date(2021, 4, 23),
+#        event="goldenhour",
+#        timezone="Europe/Berlin",
+#        longitude=13.23,
+#        latitude=52.32,
+#        return_val="api"
+#        )
