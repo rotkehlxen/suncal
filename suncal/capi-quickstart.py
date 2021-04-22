01b173f55331c9445be0d6b6e883950e1f02b75b
Franziska Witzel <franziska.witzel@gmail.com>
2021-04-22 18:51:51 +0200

    test main

--- suncal/capi-quickstart.py
+++ suncal/capi-quickstart.py
@@ -64,12 +64,12 @@ creds = get_credentials(SCOPES)
 # print(calendars)
 
 # Test main of module --------------------------------------------------------------------------------------------------
-# suncal(calendar_title="Sonne",
-#        from_date=dt.date(2021, 4, 22),
-#        to_date=dt.date(2021, 4, 23),
-#        event="goldenhour",
-#        timezone="Europe/Berlin",
-#        longitude=13.23,
-#        latitude=52.32,
-#        return_val="api"
-#        )
+suncal(calendar_title="Sonne",
+       from_date=dt.date(2021, 4, 22),
+       to_date=dt.date(2021, 4, 23),
+       event="sunset",
+       timezone="Europe/Berlin",
+       longitude=13.23,
+       latitude=52.32,
+       return_val="api"
+       )
