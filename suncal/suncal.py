a2599852a0c2572f46349a59a611de4cbe6eb22d
Franziska Witzel <franziska.witzel@gmail.com>
2021-06-12 11:32:47 +0200

    remove capi-quickstart

--- suncal/suncal.py
+++ suncal/suncal.py
@@ -139,3 +139,18 @@ def suncal(
             f"*** {event.title()} could not be calculated for the specified location. "
             f"No calendar events created. ***"
         )
+
+    if __name__ == "__main__":
+
+        suncal(
+            calendar_title="Sonne",
+            from_date=dt.date(2021, 5, 10),
+            to_date=dt.date(2021, 5, 11),
+            event="sunrise",
+            timezone="Europe/Berlin",
+            longitude=13.23,
+            latitude=52.32,
+            return_val="ics",
+            filename="blub",
+        )
+
