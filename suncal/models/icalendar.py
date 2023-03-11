026f401055d9c28a47615d5afb8dfd2e3cd09878
Franziska Witzel <franziska.witzel@gmail.com>
2023-03-11 16:52:57 +0100

    add support for all-day events in ics export, covered by unit test

--- suncal/models/icalendar.py
+++ suncal/models/icalendar.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import datetime as dt
 from typing import List
+from typing import Union
 from uuid import uuid4
 
 from pydantic import BaseModel  # pylint: disable=E0611
@@ -16,25 +17,31 @@ class VEvent(BaseModel):
     IMPORTANT: all VEvents of an icalendar have to be initialised with the same [dtstamp],
     using e.g. dt.datetime.now(dt.timezone.utc) defined previously."""
 
-    dtend: dt.datetime  # start datetime (timezone aware)
-    dtstart: dt.datetime  # end datetime (timezone aware)
+    dtend: Union[
+        dt.datetime, dt.date
+    ]  # start datetime (timezone aware) or date
+    dtstart: Union[
+        dt.datetime, dt.date
+    ]  # end datetime (timezone aware) or date
     dtstamp: dt.datetime  # datetime of ics file creation (timezone aware)
     uid: str  # unique identifier of icalendar event
     summary: str  # event title
     transp: str  # transparency of event
 
     @validator("dtend", "dtstart", "dtstamp")
-    def validate_timezone_awareness(cls, date_time):  # pylint: disable=E0213
-        assert (
-            date_time.tzinfo is not None and date_time.utcoffset() is not None
-        ), "All datetimes must be timezone-aware!"
-        return date_time
+    def validate_timezone_awareness(cls, timestamp):  # pylint: disable=E0213
+        if type(timestamp) == dt.datetime:
+            assert (
+                timestamp.tzinfo is not None
+                and timestamp.utcoffset() is not None
+            ), "All datetimes must be timezone-aware!"
+        return timestamp
 
     @staticmethod
     def fromGoogleCalEvent(ge: GoogleCalEvent, dtstamp: dt.datetime) -> VEvent:
         ical_event = VEvent(
-            dtstart=ge.start.dateTime,
-            dtend=ge.end.dateTime,
+            dtstart=ge.start.dateTime or ge.start.date,
+            dtend=ge.end.dateTime or ge.end.date,
             dtstamp=dtstamp,
             uid=f"{uuid4()}@itsalwaysbeen.photography",
             summary=ge.summary,
@@ -46,10 +53,25 @@ class VEvent(BaseModel):
 
     def to_ics(self) -> List[str]:
         """Create lines in ics file from VEvent class object."""
+
+        def ics_date_format(date: dt.date) -> str:
+            return f";VALUE=DATE:{date.strftime('%Y%m%d')}"
+
+        dtstart_str = (
+            ics_date_format(self.dtstart)
+            if type(self.dtstart) == dt.date
+            else f":{aware_datetime_to_ical_date_with_utc_time(self.dtstart)}"
+        )  # type: ignore
+        dtend_str = (
+            ics_date_format(self.dtend)
+            if type(self.dtend) == dt.date
+            else f":{aware_datetime_to_ical_date_with_utc_time(self.dtend)}"
+        )  # type: ignore
+
         ics_entry = [
             'BEGIN:VEVENT',
-            f'DTSTART:{aware_datetime_to_ical_date_with_utc_time(self.dtstart)}',
-            f'DTEND:{aware_datetime_to_ical_date_with_utc_time(self.dtend)}',
+            f'DTSTART{dtstart_str}',
+            f'DTEND{dtend_str}',
             f'DTSTAMP:{aware_datetime_to_ical_date_with_utc_time(self.dtstamp)}',
             f'UID:{self.uid}',
             f'SUMMARY:{self.summary}',
