be76d8f611fbaa3df589dcc710ce087f958a8464
Franziska Witzel <franziska.witzel@gmail.com>
2023-04-10 14:19:02 +0200

    add timezonefinder package, add unit test"

--- tests/test_astro.py
+++ tests/test_astro.py
@@ -11,6 +11,7 @@ from suncal.models.astro import MoonPhase
 from suncal.models.astro import RiseSet
 from suncal.models.astro import calculate_moon_phase
 from suncal.utils import tz_aware_dt
+from tests.test_data import CITIES
 
 
 def test_location_class():
@@ -87,59 +88,7 @@ def test_rise_set_calculations():
     date = dt.date(2023, 3, 9)
     prec = dt.timedelta(minutes=3)
 
-    # locations, date 9.3.2023, sun/moon rise & set according to timeanddate.com (tad) in local time
-    berlin = {
-        'lat': 52.520008,
-        'long': 13.404954,
-        'timezone': 'Europe/Berlin',
-        'sunrise': dt.time(6, 35),
-        'sunset': dt.time(17, 59),
-        'moonrise': dt.time(20, 17),
-        'moonset': dt.time(7, 26),
-    }
-
-    redwoodcity = {
-        'lat': 37.4848,
-        'long': -122.2281,
-        'timezone': 'US/Pacific',
-        'sunrise': dt.time(6, 28),
-        'sunset': dt.time(18, 10),
-        'moonrise': dt.time(20, 33),
-        'moonset': dt.time(7, 40),
-    }
-
-    punta_arenas = {
-        'lat': -53.163833,
-        'long': -70.917068,
-        'timezone': 'America/Santiago',
-        'sunrise': dt.time(7, 24),
-        'sunset': dt.time(20, 22),
-        'moonrise': dt.time(21, 12),
-        'moonset': dt.time(9, 33),
-    }
-    maputo = {
-        'lat': -25.966667,
-        'long': 32.583333,
-        'timezone': 'Africa/Johannesburg',
-        'sunrise': dt.time(5, 47),
-        'sunset': dt.time(18, 12),
-        'moonrise': dt.time(19, 28),
-        'moonset': dt.time(7, 15),
-    }
-
-    auckland = {
-        'lat': -36.848461,
-        'long': 174.763336,
-        'timezone': 'Pacific/Auckland',
-        'sunrise': dt.time(7, 13),
-        'sunset': dt.time(19, 49),
-        'moonrise': dt.time(20, 46),
-        'moonset': dt.time(8, 23),
-    }
-
-    cities = [berlin, redwoodcity, punta_arenas, maputo, auckland]
-
-    for city in cities:
+    for city in CITIES:
         location = Location(
             timezone=city['timezone'],
             longitude=city['long'],
